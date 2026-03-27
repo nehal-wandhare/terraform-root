cdb73cf2cdeb63c0aca90a3264675e133b0f3564
Nehal Wandhare <nehalwandhare000@gmail.com>
2026-03-27 12:14:45 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "dev-khatrnak-newtirri-tirri-bucket "
+    bucket         = "dev-khatrnak-newtirri-tirri-bucket"
     key            = "terraform.tfstate"
     region         = "ap-south-1"
     use_lockfile   = true
