154f20465f64e73a89d994c02754d01815b026e4
Nehal Wandhare <nehalwandhare000@gmail.com>
2026-03-27 12:09:38 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "khatrnak-newtirri-tirri-bucket"
-    key            = "env/${terraform.workspace}/terraform.tfstate"
+    key            = "terraform.tfstate"
     region         = "ap-south-1"
     dynamodb_table = "otp-table"
   }
