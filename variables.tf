551f03f3a6889c10af3150f9a8e3053faa099fc5
Nehal Wandhare <nehalwandhare000@gmail.com>
2026-03-27 11:03:55 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,31 +1,9 @@
-# VPC
-variable "vpc_cidr" {}
-variable "subnet_cidr" {}
-variable "vpc_name" {}
-
-# SG
-variable "sg_name" {}
-variable "allowed_ssh_cidr" {
-  type = list(string)
-}
-variable "allowed_http_cidr" {
-  type = list(string)
+variable "environment" {
+  description = "dev/test/prod"
+  type        = string
 }
 
-# Common
-variable "environment" {}
-
-# EC2
-variable "ami" {}
-variable "instance_type" {}
-variable "key_name" {}
-variable "instance_name" {}
-
-# S3
-variable "bucket_name" {}
-
-# RDS
-variable "db_class" {}
-variable "db_user" {}
-variable "db_pass" {}
-variable "db_name" {}
+variable "instance_name" {
+  description = "EC2 name"
+  type        = string
+}
